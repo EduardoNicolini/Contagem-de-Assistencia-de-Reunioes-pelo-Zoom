22283c3c06b6c9b2687446199196124346a659df
Eduardo Nicolini <dunicolini@gmail.com>
2023-11-06 00:02:04 -0300

    Update horario_reuniaop.php
    asd

--- horario_reuniaop.php
+++ horario_reuniaop.php
@@ -9,18 +9,12 @@
     $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
     echo json_encode(
-
         array(  
-            
             'dataReuniao' => $data['data'],
             'diaSemana' => $data['semana'] ?? 'Domingo',
             'horario_1' => $data['start_time'] ?? 0,
             'horario_2' => $data['end_time'] ?? 0
-            
         )
-        
     );
 
-        
-        
 ?>
