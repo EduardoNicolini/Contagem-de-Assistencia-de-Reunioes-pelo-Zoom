8897f21a89ad90db060b61653af1c58edfa23a73
Eduardo Nicolini <dunicolini@gmail.com>
2023-11-06 00:05:45 -0300

    Update dados_admin2.php
    çl

--- dados_admin2.php
+++ dados_admin2.php
@@ -25,28 +25,5 @@
             
         )
     );
-
-    // Usando operadores ternarios para simplificar o codigo. 
-
-    /* 
-        Se: (? mesmo que if) a expressão $data['total_pessoas'] for maior que 0 insere o valor na frente do ? na variavel $participants.
-        Caso o valor de $data['total_pessoas'] seja nulo ou vazio (: mesmo que else) insere o valor a frente, no caso 0
-        Ficaroa assim: ( $participants = $data['total_pessoas'] > 0 ? $data['total_pessoas'] : 0; )
-        Outra forma.
-        A expressão $data['total_pessoas'] é avaliada para o valor 0 a frente do ?? se $data['total_pessoas'] for null e insere o valor no array 'total_pessoas'
-        Do contrário será o valor da expressão $data['total_pessoas']
-        Ou pode ser desta forma ( $participants = $data['total_pessoas'] ?? 0; ) passando o valor para uma variavel;
-        
-        */
-        /*
-        while($data = $stmt->fetch(PDO::FETCH_ASSOC)){
-
-            $vetor[] = array_map('utf8_encode', $data);
-
-        }
-    
-        echo json_encode($vetor);
-    */
-        
-        
+                
 ?>
